beb5430a740857c8003937ddcc3c0e03bf20f442
Juha Kervinen <joker@iki.fi>
2014-08-12 14:45:43 +0300

    Change paths

--- script/deploy-prod.sh
+++ script/deploy-prod.sh
@@ -3,7 +3,7 @@
 # This script deploys the app updating is as submodule
 # Config:
 # root of the repository that hosts gallery as submodule 
-reporoot=/home/flowebadm/deployment/ekukka/
+reporoot=/home/flowebadm/deployment/ekukka
 #path to the submodule
 submodroot="$reporoot/ekukka_gallery"
 #root on local filesystem where the app is deployed
@@ -11,4 +11,4 @@ approot=/home/flowebadm/ekukka_gallery
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 (cd $reporoot && git submodule update)
 (cd $reporoot && git pull)
-(cp "$submodroot/*" "$approot/")
+(cp -r "$submodroot/*" "$approot/")
